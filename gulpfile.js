ff8baaed1207173826a41f9fa86634ed4e5d353a
Raz1o <tyouzu1@163.com>
2020-04-01 23:35:51 +0800

    2020-3

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
-var gulp        = require('gulp');
-var sass        = require('gulp-sass');
-var prefix      = require('gulp-autoprefixer');
+const { series, src, dest, watch, parallel } = require('gulp');
+var less        = require('gulp-less');
 var uglify      = require('gulp-uglify');
 var rename      = require('gulp-rename');
 var concat      = require('gulp-concat');  //合并文件
@@ -8,97 +7,110 @@ var changedInPlace = require('gulp-changed-in-place');//不管修改哪个文件
 var minifyHTML  = require('gulp-htmlmin');  //简化html
 var browserSync = require('browser-sync').create(); //自动刷新
 var babel       = require('gulp-babel'); //支持es6
-var sequence    = require('gulp-sequence'); //按照顺序执行
-var clean       = require('gulp-clean');  //监听
+var gulpClean       = require('gulp-clean');  //监听
+var LessAutoprefix = require('less-plugin-autoprefix');
+var minifyCSS = require('gulp-csso');
+var gutil       = require('gulp-util');  //log
+
+var autoprefix = new LessAutoprefix({ browsers: ['last 6 versions', '> 1%', 'ie 9'] });
 
 /**
- * 打包 scss 文件
+ * 打包 less 文件
  */
-gulp.task('minifysass',function(){
-        gulp.src('src/style/style.scss')
-        .pipe(sass({
-            includePaths:['scss'],
-            onError:function(e){
-                console.log(e)
-            },
-            outputStyle: 'compressed' // 压缩
+function minifyless () {
+    return src('src/style/style.less', {allowEmpty: true})
+        .pipe(less({
+            plugins: [autoprefix],
         }))
-        .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'],{cascade: true}))
+        .pipe(minifyCSS())
         .pipe(rename('style.min.css'))
-        .pipe(gulp.dest('css'))
+        .pipe(dest('dist/css'))
         .pipe(browserSync.stream());
-})
+}
 
 /***
  * 压缩 js 文件
  */
-gulp.task('minifyjs',function(){
-    gulp.src('src/js/index.js')
-        .pipe(babel())
-        .pipe(uglify())
-        .pipe(concat('index.min.js'))
-        .pipe(gulp.dest('js'))
-        .pipe(browserSync.stream());
-})
+function minifyjs(){
+    return src('src/js/index.js')
+    .pipe(babel())
+    .pipe(uglify())
+    .on('error', function(err) {
+        gutil.log(gutil.colors.red('[Error]'), err.toString());
+    })
+    .pipe(concat('index.min.js'))
+    .pipe(dest('dist/js'))
+    .pipe(browserSync.stream());
+}
+    
 
 /***
  * 拷贝 font
  */
-gulp.task('font',function(){
-    gulp.src('src/font/**')
-        .pipe(gulp.dest('font'))
-        .pipe(browserSync.stream());
-})
+function copyFont(){
+    return  src('src/font/**')
+    .pipe(dest('dist/font'))
+    .pipe(browserSync.stream());
+}
 /***
  * 拷贝 image
  */
-gulp.task('image',function(){
-    gulp.src('src/image/**')
-        .pipe(gulp.dest('image'))
+function copyImage(){
+    return src('src/image/**')
+        .pipe(dest('dist/image'))
         .pipe(browserSync.stream());
-})
+}
 
-gulp.task('minify-html', function(){
-    gulp.src('src/*.html')
+function minifyHtml(){
+    return src('src/*.html')
         .pipe(changedInPlace({firstPass: true}))
         .pipe(minifyHTML({collapseWhitespace: true}))
-        .pipe(gulp.dest(''))
+        .pipe(dest('dist'))
         .pipe(browserSync.stream());
-});
+}
 
-gulp.task('clean', function () {
-    gulp.src(['./css/*.css','./js/*.js'], {read: false})
-        .pipe(clean());
-});
+function clean() {
+    return src('dist', {read: false, allowEmpty:true})
+        .pipe(gulpClean());
+}
 
 var files = [
-    './css/*.css',
-    './js/*.js'
+    './dist/css/*.css',
+    './dist/js/*.js'
 ];
 
-gulp.task('browser-sync', function(){
-    browserSync.init(files,{
+function browserSyncTask(){
+    return browserSync.init(files,{
         server: {
-            baseDir: './'
+            baseDir: './dist'
         },
         port: 9999
     });
-});
+}
 
-gulp.task('watch',function(){
-    gulp.watch('src/style/*.scss',['minifysass'])
-    gulp.watch('src/js/index.js',['minifyjs'])
-    gulp.watch('src/index.html',['minify-html'])
-    gulp.watch('src/font/**',['font'])
-    gulp.watch('src/image/**',['image'])
-
-})
-
-
+function watchTask() {
+    watch('src/style/*.less',minifyless)
+    watch('src/js/index.js',minifyjs)
+    watch('src/index.html',minifyHtml)
+    watch('src/font/**',copyFont)
+    watch('src/image/**',copyImage)
+}
 
 /**
  * default 任务
  */
-
 //develop
-gulp.task('default', sequence('clean','minifyjs', 'minifysass','minify-html', 'watch','browser-sync'));
+
+// exports.build = build;
+exports.default = series(
+    clean, 
+    parallel(
+        copyImage,
+        copyFont,
+        minifyjs,
+        minifyless,
+        minifyHtml,
+        watchTask,
+        browserSyncTask
+    )
+);
